e5f277bf03abd69404c86152a60266549039a710
jwonlee95 <96804163+jwonlee95@users.noreply.github.com>
2022-03-23 00:21:37 +0900

    init

---
+++ UltimatePortfolio/UltimatePortfolio/UltimatePortfolioApp.swift
@@ -0,0 +1,17 @@
+//
+//  UltimatePortfolioApp.swift
+//  UltimatePortfolio
+//
+//  Created by Jae Won Lee on 2022/03/23.
+//
+
+import SwiftUI
+
+@main
+struct UltimatePortfolioApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
