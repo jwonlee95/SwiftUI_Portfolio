4ac811ed0717f351c32652f085527a96957fb723
jwonlee95 <96804163+jwonlee95@users.noreply.github.com>
2022-03-30 05:07:39 +0900

    HomeView UI

---
+++ UltimatePortfolio/UltimatePortfolio/Color-Additions.swift
@@ -0,0 +1,14 @@
+//
+//  Color-Additions.swift
+//  UltimatePortfolio
+//
+//  Created by Jae Won Lee on 2022/03/30.
+//
+
+import SwiftUI
+
+extension Color {
+    //colors from UIKit
+    static let systemGroupedBackground = Color(UIColor.systemGroupedBackground)
+    static let secondarySystemGroupedBackground = Color(UIColor.secondarySystemGroupedBackground)
+}
